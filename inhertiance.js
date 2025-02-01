790651f89e0e69480ed6bfe107aa7f72eaf91479
Neomi Averbach <6739852@gmail.com>
2025-02-02 00:23:32 +0200

    Update inhertiance.jsא

--- inhertiance.js
+++ inhertiance.js
@@ -1,97 +1,91 @@
-function HtmlElement(type,textContext="")
-{
-    this.id=HtmlElement.prototype.counter++
-    this.type=type
-    this.textContext=textContext
-    if(new.target)
-         throw new Error("can't create an instance of abstract class")
-}
-HtmlElement.prototype.counter=0
-HtmlElement.prototype.render=function(){
-    let elem=document.createElement(this.type)
-    elem.id="elem"+this.id
-    let context=document.createElement("p")
-    context.innerHTML=this.textContext
-    elem.appendChild(context)
-    if(this instanceof ImageElement)
-    {
-        elem.src=this.src
-        elem.alt=this.textContext
+function HtmlElement(type, textContext = "") {
+    if (new.target === HtmlElement) {
+        throw new Error("can't create an instance of abstract class");
     }
-    else if(this instanceof SelectElement)
-    {
-        for (let i = 0; i < this.options.length; i++) {
-            let option = document.createElement("option");
-            option.value = this.options[i]
-            option.textContent = this.options[i]
-            elem.appendChild(option);
-        }
-    }
-    return elem
+    this.id = HtmlElement.prototype.counter++;
+    this.type = type;
+    this.textContext = textContext;
 }
 
+HtmlElement.prototype.counter = 0;
 
-function ImageElement(src,alt)
-{
-    HtmlElement.call(this, "img",alt);
-    this.src=src
-}
-ImageElement.prototype=Object.create(HtmlElement.prototype)
-ImageElement.prototype.constructor=ImageElement
+HtmlElement.prototype.render = function () {
+    let elem = document.createElement(this.type);
+    elem.id = "elem" + this.id;
+    let context = document.createElement("p");
+    context.innerHTML = this.textContext;
+    elem.appendChild(context);
+    return elem;
+};
 
-function SelectElement(options)
-{
-    HtmlElement.call(this,"select")
-    this.options=options.split(",")
-
-}
-SelectElement.prototype=Object.create(HtmlElement.prototype)
-SelectElement.prototype.constructor=SelectElement
-
-function generateElement()
-{
-    try{
-        let type=document.querySelector("#elemType").value
-        let context=document.querySelector("#elemContext").value
-        let element=new HtmlElement(type,context)
-        let elem=element.render()
-        document.body.querySelector("#image1").append(elem)
-       
-    }
-    catch(error)
-    {
-        console.log(error.message)
-    }
+function ImageElement(src, alt) {
+    HtmlElement.call(this, "img", alt);
+    this.src = src;
 }
 
-function generateImage()
-{
-    try{
-        let src=document.querySelector("#elemSrc").value
-        let alt=document.querySelector("#elemAlt").value
-        let element=new ImageElement(src,alt)
-        let elem=element.render()
-        document.body.querySelector("#element1").append(elem)
-    }
-    catch(error)
-    {
-        console.log(error.message)
-    }
+ImageElement.prototype = Object.create(HtmlElement.prototype);
+ImageElement.prototype.constructor = ImageElement;
 
+ImageElement.prototype.render = function () {
+    let elem = document.createElement("img");
+    elem.id = "elem" + this.id;
+    elem.src = this.src;
+    elem.alt = this.textContext;
+    return elem;
+};
 
+function SelectElement(options) {
+    HtmlElement.call(this, "select");
+    this.options = options.split(",");
 }
 
-function generateSelect()
-{
-    try
-    {
-        let options=document.querySelector("#select").value
-        let element=new SelectElement(options)
-        let elem=element.render()
-        document.body.querySelector("#select1").append(elem)
+SelectElement.prototype = Object.create(HtmlElement.prototype);
+SelectElement.prototype.constructor = SelectElement;
+
+SelectElement.prototype.render = function () {
+    let elem = document.createElement("select");
+    elem.id = "elem" + this.id;
+    for (let i = 0; i < this.options.length; i++) {
+        let option = document.createElement("option");
+        option.value = this.options[i];
+        option.textContent = this.options[i];
+        elem.appendChild(option);
     }
-        catch(error)
-    {
-        console.log(error.message)
+    return elem;
+};
+
+// פונקציות יצירת אלמנטים
+function generateElement() {
+    try {
+        let type = document.querySelector("#elemType").value;
+        let context = document.querySelector("#elemContext").value;
+        let element = new HtmlElement(type, context);
+        let elem = element.render();
+        document.body.querySelector("#image1").append(elem);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function generateImage() {
+    try {
+        let src = document.querySelector("#elemSrc").value;
+        let alt = document.querySelector("#elemAlt").value;
+        let element = new ImageElement(src, alt);
+        let elem = element.render();
+        document.body.querySelector("#element1").append(elem);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function generateSelect() {
+    try {
+        let options = document.querySelector("#select").value;
+        let element = new SelectElement(options);
+        let elem = element.render();
+        document.body.querySelector("#select1").append(elem);
+    } catch (error) {
+        console.log(error.message);
     }
 }
